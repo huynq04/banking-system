76bd827b19e1b58ba835617e14ce4df92b95f0b6
Huy Nguyen <huynq.b22cn391@stu.ptit.edu.vn>
2025-01-19 23:17:58 +0700

    Exception handling and Error Formating

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,31 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponseTrait;
+use Error;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponseTrait;
     /**
      * A list of exception types with their corresponding custom log levels.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     * @var array<class-string<Throwable>, \Psr\Log\LogLevel::*>
      */
     protected $levels = [
         //
@@ -19,7 +35,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -44,5 +60,103 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e): \Illuminate\Http\Response|JsonResponse|Response|RedirectResponse
+    {
+        if ($request->expectsJson() || Str::contains($request->path(), 'api')) {
+
+            Log::error($e);
+
+            if ($e instanceof AuthenticationException) {
+                $statusCode = Response::HTTP_UNAUTHORIZED;
+                return $this->apiResponse([
+                    'message' => "Unauthenticated or Token expired, please try to login again",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ], $statusCode);
+            }
+
+            if ($e instanceof NotFoundHttpException) {
+                return $this->apiResponse([
+                    'message' => $e->getMessage(),
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $e->getStatusCode(),
+                ], $e->getStatusCode());
+            }
+
+            if ($e instanceof ValidationException) {
+                $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+                return $this->apiResponse([
+                    'message' => "Validation failed",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                    'errors' => $e->errors(),
+                ], $statusCode);
+            }
+
+            if ($e instanceof ModelNotFoundException) {
+                $statusCode = Response::HTTP_NOT_FOUND;
+                return $this->apiResponse([
+                    'message' => "Resource could not be found",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ], $statusCode);
+            }
+
+            if ($e instanceof UniqueConstraintViolationException) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "Duplicate entry found",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+            if ($e instanceof QueryException) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "Could not execute query",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+//            if ($e instanceof MethodNotAllowedHttpException) {
+//                return $this->apiResponse([
+//                    'message' => $e->getMessage(),
+//                    'success' => false,
+//                    'exception' => $e,
+//                    'error_code' => Response::HTTP_METHOD_NOT_ALLOWED,
+//                ], Response::HTTP_METHOD_NOT_ALLOWED);
+//            }
+
+            if ($e instanceof Exception) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "We could not handle your request, please try again later",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+
+            if ($e instanceof Error) {
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                return $this->apiResponse([
+                    'message' => "We could not handle your request, please try again later",
+                    'success' => false,
+                    'exception' => $e,
+                    'error_code' => $statusCode,
+                ]);
+            }
+        }
+        return parent::render($request, $e);
     }
 }
